4ee009e59904a7c17c99033895470799e0b5d1be
todesking <discommunicative@gmail.com>
2010-04-24 23:11:38 +0900

    reconstructing

--- solver.rb
+++ solver.rb
@@ -9,125 +9,31 @@ class Solver
     return [:put,availables.first]
   end
   def available_pos board
-    result=[]
-    each_horizonal(board) {|line,y|
-      match=/0E+P|PE+0/.match(line)
-      unless match.nil?
-        match.length.times{|i|
-          result.push match[i][0]=='0'[0] ? [match.begin(i),y] : [match.end(i)-1,y]
-        }
-      end
-    }
-    each_vertical(board) {|line,x|
-      match=/0E+P|PE+0/.match(line)
-      unless match.nil?
-        match.length.times{|i|
-          result.push match[i][0]=='0'[0] ? [x,match.begin(i)] : [x,match.end(i)-1]
-        }
-      end
-    }
-    each_diagonal_right(board) {|line,x,y|
-      match=/0E+P|PE+0/.match(line)
-      unless match.nil?
-        match.length.times{|i|
-          result.push match[i][0]=='0'[0] ? [x,y+match.begin(i)] : [x+match.end(i)-1,y+match.end(i)-1]
-        }
-      end
-    }
-    each_diagonal_left(board) {|line,x,y|
-      match=/0E+P|PE+0/.match(line)
-      unless match.nil?
-        match.length.times{|i|
-          result.push match[i][0]=='0'[0] ? [x,y+match.begin(i)] : [x,y-match.end(i)-1]
-        }
-      end
-    }
-    return result.uniq
+    []
   end
   def score(board,pos)
-    info=get_info(board,pos)
-    return -1 if info[:E]<1
-    return info[:X]*0.3+info[:P]*-0.1+rand
+    0
   end
   def get_info board,pos
     blank=0
     player=0
     enemy=0
     block=0
-    dirs=[[-1,0],[1,0],[0,-1],[0,1],[1,1],[1,-1],[-1,1],[-1,-1]]
-    dirs.each{|dir|
-      case block_at(board,[pos[0]+dir[0],pos[1]+dir[1]])
-      when 'X'
-        block+=1
-      when 'E'
-        enemy+=1
-      when 'P'
-        player+=1
-      when '0'
-        blank+=1
-      end
-    }
     return :blank=>blank,:P=>player,:E=>enemy,:X=>block
   end
-  def block_at board,pos
-    return 'X' if pos[0] < 0 || width(board)<=pos[1]
-    return 'X' if pos[1] < 0 || height(board)<=pos[1]
-    return board[pos[1]][pos[0],1]
-  end
-  def each_horizonal board
-    board.each_with_index{|line,y| yield line,y}
-  end
-  def each_vertical board
-    board.first.length.times{|x|
-      line=''
-      board.length.times{|y|
-        line+=board[y][x,1]
-      }
-      yield line,x
-    }
-  end
-  def each_diagonal_right board
-    return if width(board)<3 || height(board)<3
-    # \ (upper)
-    (0...width(board)-2).each{|x|
-      line=''
-      (0...[width(board)-x,height(board)].min).each{|i|
-        line+=board[i][x+i,1]
-      }
-      yield line,x,0
-    }
-    # \ (downer)
-    (0..height(board)-3).each{|y|
-      line=''
-      (0...[width(board),height(board)-y].min).each{|i|
-        line+=board[y+i][i,1]
-      }
-      yield line,0,y
-    }
-  end
-  def each_diagonal_left board
-    return if width(board)<3 || height(board)<3
-    # / (upper)
-    (0...width(board)-2).each{|x|
-      line=''
-      ([width(board)-x,height(board)].min-1..0).each{|i|
-        line+=board[i][x+i,1]
-      }
-      yield line,x,0
-    }
-    # / (downer)
-    (0..height(board)-3).each{|y|
-      line=''
-      ([width(board),height(board)-y].min-1..0).each{|i|
-        line+=board[y+i][i,1]
-      }
-      yield line,0,y
-    }
-  end
-  def width(board)
-    board.first.length
-  end
-  def height(board)
-    board.length
+  class Board
+    def initialize board,reversible
+      @board=board
+      @reversible=reversible
+    end
+    def w
+    end
+    def h
+    end
+    def [](x,y)
+    end
+    def possible_pos
+      []
+    end
   end
 end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,65 +1,46 @@
 require File.join(File.dirname(__FILE__),'..','solver.rb')
 
 describe Solver do
-  before do
-    @s=Solver.new
+end
+
+describe Solver::Board do
+  def when_board_is b
+    @board=Solver::Board.new b,0
   end
-  describe '#available_pos' do
-    describe 'when none to reverse' do
-      it 'returns empty if all blocked' do
-        @s.available_pos(['X']).should be_empty
-        @s.available_pos(['XXX','XXX']).should be_empty
-      end
-      it 'returns empty if all block is empty' do
-        @s.available_pos(['0000','0000','0000']).should be_empty
-      end
-      it 'returns empty if board is fullfilled' do
-        @s.available_pos(['PPP','PEP','EEE']).should be_empty
-      end
-      it 'returns empty if could not reverse any enemys block' do
-        @s.available_pos(['PP0']).should be_empty
-        @s.available_pos(['EP0']).should be_empty
-      end
+  describe '#w' do
+    subject { @board.w }
+    it 'should return width of the board' do
+      pending
+      when_board_is [ '000' ]
+      should == 3
     end
-    describe 'when some reversible positions exists' do
-      it 'should return horizontal availables' do
-        @s.available_pos(['PE0']).should == [[2,0]]
-        @s.available_pos(['PEE00P']).should == [[3,0]]
-        @s.available_pos(['0EP0']).should == [[0,0]]
-      end
-      it 'should return vertical availables' do
-        @s.available_pos(['P','E','0']).should == [[0,2]]
-        @s.available_pos(['0','E','P']).should == [[0,0]]
-      end
-      it 'should return diagonal availables' do
-        @s.available_pos(['P00','0E0','000']).should == [[2,2]]
-        @s.available_pos(['000','0E0','00P']).should == [[0,0]]
-        @s.available_pos(<<-B.split("\n").map(&:strip)).should == [[3,4]]
-        0000
-        0000
-        0P00
-        00E0
-        0000
-        B
-        @s.available_pos(<<-B.split("\n").map(&:strip)).should == [[4,2]]
-        00P00
-        000E0
-        00000
-        B
-        @s.available_pos(<<-B.split("\n").map(&:strip)).should == [[0,2]]
-        00P00
-        0E000
-        00000
-        B
-      end
-      it 'should return various availables' do
-        pending
-        @s.available_pos(<<-B.split("\n").map(&:strip)).sort_by{|a,b|a*1000+b}.should == [[1,2],[2,1],[2,2]]
-        PP000
-        PE000
-        00000
-        B
-      end
+  end
+  describe '#h' do
+    subject { @board.h }
+    it 'should return height of board' do
+      pending
+      when_board_is [ '000' ]
+      should == 1
+    end
+  end
+  describe '#self[]' do
+    it 'should return cell state of the position' do
+      pending
+      when_board_is [ '000', 'PEP' ]
+      @board[0,0].should == '0'
+      @board[1,1].should == 'E'
+      @board[-1,0].should == 'X'
+      @board[0,-1].should == 'X'
+      @board[3,0].should == 'X'
+      @board[0,2].should == 'X'
+    end
+  end
+  describe '#possible_pos' do
+    subject { @board.possible_pos }
+    it 'should return possible positions for next turn'do
+      pending
+      when_board_is ['PE0']
+      should == [ [2,0] ]
     end
   end
 end
