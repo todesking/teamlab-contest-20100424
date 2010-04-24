d72c495989de4484a6bc97e15bcf0223a9938d5d
todesking <discommunicative@gmail.com>
2010-04-24 14:47:36 +0900

    Solver#available_pos: add spec

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -5,12 +5,21 @@ describe Solver do
     @s=Solver.new
   end
   describe '#available_pos' do
-    it 'returns empty if all blocked' do
-      @s.available_pos(['X']).should be_empty
-      @s.available_pos(['XXX','XXX']).should be_empty
-    end
-    it 'returns empty if none to reverse(case: all empty board)' do
-      @s.available_pos(['0000','0000','0000']).should be_empty
+    describe 'none to reverse' do
+      it 'returns empty if all blocked' do
+        @s.available_pos(['X']).should be_empty
+        @s.available_pos(['XXX','XXX']).should be_empty
+      end
+      it 'returns empty if all block is empty' do
+        @s.available_pos(['0000','0000','0000']).should be_empty
+      end
+      it 'returns empty if board is fullfilled' do
+        @s.available_pos(['PPP','PEP','EEE']).should be_empty
+      end
+      it 'returns empty if could not reverse any enemys block' do
+        @s.available_pos(['PP0']).should be_empty
+        @s.available_pos(['EP0']).should be_empty
+      end
     end
   end
 end
